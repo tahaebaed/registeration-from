dc2b2d8e2b78762dd71f873937a7a6328d42096e
Taha Ebaed <tahaebaed2@gmail.com>
2022-07-17 20:10:23 +0200

    remove unused functions

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 
